034712fb6741e917161b9d44c65a00a934b64de3
eu <diego_thegarden@hotmail.com>
2022-10-19 20:55:26 +0200

    server and db connection

---
+++ README.md
@@ -0,0 +1,1 @@
+API for a issue tracker web APP.
---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config(); //Dev
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const dbUrl = process.env.DB_URL;
+
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Mongo connection open");
+  })
+  .catch((err) => {
+    console.log("Mongo connection error");
+    console.log(err);
+  });
+
+// For testing purposes
+app.get("/", (req, res) => {
+  res.send("<h2>It's Working!</h2>");
+});
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
