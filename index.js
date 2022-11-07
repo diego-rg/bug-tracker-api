7fe0261176ce41ecbcd31397084d68f7afc66554
eu <diego_thegarden@hotmail.com>
2022-11-07 20:32:09 +0100

    cors error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://drg-bugtracker.vercel.app/"],
+    origin: ["https://drg-bugtracker.vercel.app/", "http://localhost:3000"],
     credentials: true,
   })
 );
