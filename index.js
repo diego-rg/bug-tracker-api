7de5662596875567a75cd1c4c4376acc26d3274d
eu <diego_thegarden@hotmail.com>
2022-11-07 21:06:50 +0100

    deploy 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://drg-bugtracker.vercel.app/", "http://localhost:3000"],
+    origin: "https://drg-bugtracker.vercel.app/",
     credentials: true,
   })
 );
