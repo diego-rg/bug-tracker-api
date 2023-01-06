d7d01f9adff99380b56615ea9044205dafb84839
eu <diego_thegarden@hotmail.com>
2023-01-06 22:04:04 +0100

    fixed: github auth bug when user has no public email

--- auth/github.js
+++ auth/github.js
@@ -3,6 +3,7 @@ const GitHubStrategy = require("passport-github2").Strategy;
 
 const User = require("../models/user");
 
+// Importante: o email de GitHub ten que estar configurado como público na súa conta, senón devolve null e non crea o user
 passport.use(
   new GitHubStrategy(
     {
@@ -12,14 +13,16 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
+        console.log(profile);
         const user = await User.findOne({ githubId: profile.id });
+        console.log(user);
         if (user) {
           cb(null, user);
         } else {
           const user = await new User({
             name: profile.displayName,
             githubId: profile.id,
-            email: profile.email,
+            email: profile._json.email || "noemail@noemail.noemail",
           }).save();
           cb(null, user);
         }
